6822cc79d6e4659bcef43d7602e9a64007f35e68
Hyunsuk Lee <leehs27th@gmail.com>
2021-07-20 18:27:16 +0900

    PlayerController rotate player

--- Assets/02.Scripts/PlayerController.cs
+++ Assets/02.Scripts/PlayerController.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private PlayerControllerAction playerControllerAction = new PlayerControllerAction();
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        playerControllerAction = new PlayerControllerAction();
+        playerControllerAction.Enable();
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Debug.Log(Cursor.lockState);
+        if(Mouse.current != null)
+        {
+            
+            Vector2 mouseMovement = playerControllerAction.Simple.Look.ReadValue<Vector2>() * Time.deltaTime * 5;
+            Vector3 eulerAngle = transform.eulerAngles;
+
+            eulerAngle.y += mouseMovement.x * 0.5f;
+            eulerAngle.x += mouseMovement.y * 0.5f;
+
+            transform.eulerAngles = eulerAngle;
+        }
+
+
     }
+
+
 }
