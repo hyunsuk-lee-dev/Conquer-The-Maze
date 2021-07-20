ce6b58bcb60461ce64e10e54e107688d6f743113
Hyunsuk Lee <leehs27th@gmail.com>
2021-07-20 18:06:51 +0900

    Remove unused asset

--- Assets/02.Scripts/MazeGenerator.cs
+++ Assets/02.Scripts/MazeGenerator.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 
 public class MazeGenerator : MonoBehaviour
 {
-    private const float wallLength = 30;
+    private const float wallLength = 100;
 
     [SerializeField]
     private GameObject horizontalWallPrefab;
@@ -33,10 +33,10 @@ public class MazeGenerator : MonoBehaviour
 
         for(int i = 0; i < size; i++)
         {
-            CreateObject(horizontalWallPrefab, i * wallLength, 0);
-            CreateObject(horizontalWallPrefab, i * wallLength, size * wallLength);
-            CreateObject(verticalWallPrefab, 0, i * wallLength);
-            CreateObject(verticalWallPrefab, size * wallLength, i * wallLength);
+            CreateObject(horizontalWallPrefab, i * wallLength, 0, $"Wall Left");
+            CreateObject(horizontalWallPrefab, i * wallLength, size * wallLength, $"Wall Right");
+            CreateObject(verticalWallPrefab, 0, i * wallLength, $"Wall Up");
+            CreateObject(verticalWallPrefab, size * wallLength, i * wallLength, $"Wall Down");
         }
 
         for(int i = 0; i < size; i++)
@@ -45,20 +45,23 @@ public class MazeGenerator : MonoBehaviour
             {
                 if(maze.HorizontalWalls[i, j - 1])
                 {
-                    horizontalWalls[i, j - 1] = CreateObject(horizontalWallPrefab, i * wallLength, j * wallLength);
+                    horizontalWalls[i, j - 1] = CreateObject(horizontalWallPrefab, i * wallLength, j * wallLength, $"Block");
                 }
-
                 if(maze.VerticalWalls[j - 1, i])
                 {
-                    verticalWalls[j - 1, i] = CreateObject(verticalWallPrefab, j * wallLength, i * wallLength);
+                    verticalWalls[j - 1, i] = CreateObject(verticalWallPrefab, j * wallLength, i * wallLength, $"Block");
                 }
             }
         }
     }
 
-    private GameObject CreateObject(GameObject prefab, float x, float y)
+    private GameObject CreateObject(GameObject prefab, float x, float y, string name = "")
     {
         GameObject createdObject = Instantiate(prefab);
+        if(!string.IsNullOrEmpty(name))
+        {
+            createdObject.name = name;
+        }
         RectTransform rectTransform = createdObject.GetComponent<RectTransform>();
         rectTransform.SetParent(canvas);
         rectTransform.anchoredPosition = new Vector2(x, y);
